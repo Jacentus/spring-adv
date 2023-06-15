d44ab99953b225cc7e8c544baeacc79903038ec8
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2023-06-15 11:19:52 +0200

    ss

--- chat/src/main/java/pl/training/chat/ChatController.java
+++ chat/src/main/java/pl/training/chat/ChatController.java
@@ -1,14 +1,24 @@
 package pl.training.chat;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@Log
+@RequiredArgsConstructor
 public class ChatController {
 
+    private final TimeProvider timeProvider;
+
     @MessageMapping("/chat")
+    @SendTo("/main-room")
     public ChatMessageDto onMessage(ChatMessageDto chatMessageDto) {
-        var response = chatMessageDto.withTimestamp()
+        var response = chatMessageDto.withTimestamp(timeProvider.getTime());
+        log.info("New message: " + chatMessageDto);
+        return response;
     }
 
 }
